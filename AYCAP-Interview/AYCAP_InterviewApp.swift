a117b77ad33b8d0adec1ec8956201f868b5f67a4
CHCH <49768100+CHCH2538@users.noreply.github.com>
2022-06-03 06:19:17 +0700

    Initialize

---
+++ AYCAP-Interview/AYCAP_InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AYCAP_InterviewApp.swift
+//  AYCAP-Interview
+//
+//  Created by Pongpubate Charoensinphutthakhun on 3/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct AYCAP_InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
